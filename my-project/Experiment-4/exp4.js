30fa283901943a55d598bb005cdff705eceb6b92
Krish307sharma <krishsharma1907@gmail.com>
2025-09-30 19:45:38 +0530

    lab mst project

--- my-project/Experiment-4/exp4.js
+++
@@ -1,7 +0,0 @@
-
-const textarea = document.getElementById("myTextarea");
-const counter = document.getElementById("charCount");
-
-textarea.addEventListener("input", () => {
-  counter.textContent = textarea.value.length;
-});
